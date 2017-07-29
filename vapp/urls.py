9575ff9234dc695f11b193218fb2e626c7305e74
Suhail vs <suhailvs@gmail.com>
2017-07-29 22:09:42 +0530

    updated for work with django 1.11

--- vapp/urls.py
+++ vapp/urls.py
@@ -1,12 +1,14 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.contrib import auth
 from django.contrib.auth import views as auth_views
-urlpatterns = patterns('vapp.views',
-	url(r'^$', 'home', name='home'),
-	url(r'^delete/(?P<pk>\d+)/$', 'home', name='delete_item'),
-	url(r'^vote/$', 'vote_item', name='vote_item'),
+from . import views as vapp_views
+
+urlpatterns = [
+	url(r'^$', vapp_views.home, name='home'),
+	url(r'^delete/(?P<pk>\d+)/$', vapp_views.home, name='delete_item'),
+	url(r'^vote/$', vapp_views.vote_item, name='vote_item'),
 
 	url(r'^accounts/login/$', auth_views.login, {'template_name': 'login.html'}),
 	url(r'^logout/$', auth_views.logout,name="logout"),
 	url(r'^pchange/$', auth_views.password_change, {'post_change_redirect':'/'},name='password_change'),
-)
+]
